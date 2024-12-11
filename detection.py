21b3ea023023d6f1108d34c80794d6e538de9e33
Florine Kieraga <florine.kieraga@epita.fr>
2024-12-11 15:02:40 +0100

    [fix] change the cam

--- detection.py
+++ detection.py
@@ -6,15 +6,19 @@ cv2.startWindowThread()
 
 model = YOLO('yolov8n.pt')
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+picam2.preview_configuration.main.size = (1280, 720)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
+picam2.configure("preview")
 picam2.start()
 
 print("Starting the detection")
 while True:
     img = picam2.capture_array()
-    results = model.predict(source=img, show=True)
-    print(results)
+    results = model(source=img)
+    annoted_img = results[0].plot()
+    cv2.imshow("Detection", annoted_img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-
-cap.release()
 cv2.destroyAllWindows()
