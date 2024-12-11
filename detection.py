70cb68399f10d5884be2eddc1de97bd768d77fe6
Florine Kieraga <florine.kieraga@epita.fr>
2024-12-11 14:56:36 +0100

    [fix] change to picamera

--- detection.py
+++ detection.py
@@ -1,17 +1,20 @@
 from ultralytics import YOLO
+from picamera2 import Picamera2
 import cv2
 
-model = YOLO('yolov8n.pt')  # Load model
+cv2.startWindowThread()
 
-cap = cv2.VideoCapture(0)  # Webcam
+model = YOLO('yolov8n.pt')
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+picam2.start()
+
+print("Starting the detection")
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    results = model.predict(source=frame, show=True)  # Inference
-    cv2.imshow("Pedestrian Detection", frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
+    img = picam2.capture_array()
+    results = model.predict(source=img, show=True)
+    print(results)
+
 
 cap.release()
 cv2.destroyAllWindows()
