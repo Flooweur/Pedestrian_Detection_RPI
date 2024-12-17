81bbb7b7d8c368c1d199cf70c4aa99b77be58e70
Samy AMINE <samyaminework@gmail.com>
2024-12-17 11:28:59 +0100

    CLEANING: quantization

--- quantization/quantization.py
+++ quantization/quantization.py
@@ -1,21 +1,3 @@
-# from ultralytics import YOLO
-# from onnxruntime.quantization import quantize_dynamic, QuantType
-
-# # Exports the trained weights to onnx:
-# #model = YOLO("training_output/runs/detect/train/weights/model_pruned.pt")
-# #model.export(format='onnx')
-
-# # Quantize the model
-# #model_input = "training_output/runs/detect/train/weights/best.onnx"
-# model_input = "model_pruned_preprocessed.onnx"
-# model_output = "training_output/runs/detect/train/weights/best_quantized_and_pruned.onnx"
-# quantize_dynamic(model_input, model_output, weight_type=QuantType.QUInt8)
-
-
-
-
-
-
 import subprocess
 import time
 import argparse
